e46ad0fa32e6beb85b3e826ab5ef9d04825f51b3
Mark <hendriksen-mark@hotmail.com>
2025-06-10 16:59:42 +0200

    Update index.spec.ts

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,5 @@
 import plugin from './index';
+import { expect, jest, it, describe } from '@jest/globals';
 
 describe('homebridge-http-lightbulb plugin', () => {
   it('should export a function', () => {
